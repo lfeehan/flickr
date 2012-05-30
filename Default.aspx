9b1c81f41008a5fb7f176294e233fd629ef8d0ff
lfeehan <lenthesecond@yahoo.com>
2012-05-30 17:00:09 +0100

    skeleton of project

---
+++ Default.aspx
@@ -0,0 +1,95 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="Flick._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+
+
+    <div>
+    <br />
+    <div class = 'container'>
+
+        <asp:Label style="position:relative;" ID="resultsLocation" runat="server" Text=""></asp:Label>
+
+        <div id="droppable" class="savearea">
+        <div style="margin:0 auto;">
+            </div>
+                <h2 style="margin-top: 5px;">Drag Here</h2>
+	            <p>...to save</p>
+                
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+            DataSourceID="SqlDataSource1" 
+            EmptyDataText="There are no data records to display." 
+                        onselectedindexchanged="GridView1_SelectedIndexChanged"
+                        Width="100px"
+                        GridLines="None">
+            <Columns>
+          
+            <asp:TemplateField HeaderText="recent" >
+                <ItemTemplate>
+                     <a id="A1" class="group" href='<%# Eval("mainimg")%>' runat="server">
+                        <img src='<%# Eval("image")%>' runat="server" style="height: 50%; width: 50%;">
+                    </a>
+                </ItemTemplate>
+            </asp:TemplateField>
+
+            </Columns>
+        </asp:GridView>
+
+            
+            </div>
+        </div>
+    
+    </div>
+    <div>
+    
+     
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+            ConnectionString="<%$ ConnectionStrings:DB_42245_imagetagConnectionString1 %>" 
+            ProviderName="<%$ ConnectionStrings:DB_42245_imagetagConnectionString1.ProviderName %>" 
+            SelectCommand="SELECT TOP 6 [image], [mainimg], [tag], [id] FROM [saves] Order BY [id] DESC"></asp:SqlDataSource>
+
+        <div style="display:none;">
+        <asp:Panel ID="Panel2" runat="server" DefaultButton="Button1" >
+            <asp:TextBox ID="linkHidden" runat="server"></asp:TextBox>
+            <asp:TextBox ID="thumbHidden" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnCommand="Button1_Click"  />
+        </asp:Panel>
+        </div>
+
+    </div>
+    <div class="csharp">
+        <asp:Panel ID="Panel1" runat="server" DefaultButton="searchButton" >
+            <p>
+                <asp:TextBox ID="searchBox" class="textbox" runat="server" Text="enter a search term"  Width="161px" ></asp:TextBox>
+            </p>
+            <p>
+                <asp:Button ID="searchButton" runat="server" Text="Search Flickr..." OnCommand="leftButton_Click" />
+            </p>
+        </asp:Panel>
+    </div>
+    <br />
+    <div class="about">
+        <h3>
+            About:
+        </h3>
+            <p>
+            Demo exercise in parsing XML using both client and server side code.
+            Implementation of JQuery_UI using 'draggable' & 'droppable' to 
+            create an interface.
+            <br />
+
+            <ul>
+                <li>Search for images asychronously from flickr <b>Ajax</b></li>
+                <li>Parse the xml server side for links and images <b>C#</b>,<b> DOM </b></li>
+                <li>Click and drag resulting thumbnails to sort <b>Jquery_Ui</b></li>
+                <li>Drag & drop to save to database <b>Javascript</b> & <b>SQL</b></li>
+                <li>All done as a learning exercise in <b>.net 4</b></li>
+                <li>Click saved images to view full size <b>Fancybox for JQuery</b></li>
+            </ul>
+        </p>
+    
+    </div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace Flick
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        static Random r = new Random();
+        protected void leftButton_Click(object sender, CommandEventArgs e)
+        {
+            
+            {
+                string tags = searchBox.Text;
+                tags = cleanTags(tags);
+                string rss = "http://api.flickr.com/services/feeds/photos_public.gne?tags=" + tags + "&format=rss2";
+
+                WebRequest WebRequestObject = HttpWebRequest.Create(rss);
+                WebResponse ResponseObject = WebRequestObject.GetResponse();
+                StreamReader reader = new StreamReader(ResponseObject.GetResponseStream());
+
+                XmlDocument doc = new XmlDocument();
+                doc.Load(reader);
+                //namespace for tags with 'media:' eg media:thumbnail
+                XmlNamespaceManager xmlnsManager = new XmlNamespaceManager(doc.NameTable);
+                xmlnsManager.AddNamespace("prefix", "http://search.yahoo.com/mrss/");
+
+                XmlNodeList imageList = doc.SelectNodes("/rss/channel/item");
+                int classNum = 1;
+                resultsLocation.Text = "";
+                foreach (XmlNode item in imageList)
+                {
+
+                    XmlNode link = item.SelectSingleNode("prefix:thumbnail", xmlnsManager);
+                    if (link != null)
+                    {
+                        XmlNode full = item.SelectSingleNode("prefix:content", xmlnsManager);
+
+                        int top = r.Next(400);
+                        int left = r.Next(550);
+
+                        resultsLocation.Text += "<DIV savelink = " + full.Attributes["url"].Value + " savethumb=" + link.Attributes["url"].Value + " id='drag" + classNum + "' STYLE='position:absolute; float:none; top:" + top + "px; left:" + left;
+                        resultsLocation.Text += "px;' onmouseover='this.style.zIndex = 10' onmouseout='this.style.zIndex = 0'  >";
+
+                        classNum++;
+
+                        resultsLocation.Text += "<img src = '";
+                        resultsLocation.Text += link.Attributes["url"].Value;
+                        resultsLocation.Text += "'/></div>";
+                    }
+
+                }
+            }
+            
+
+        }
+
+        private string cleanTags(string tags)
+        {
+            //take user input and validate, separate words with comma as per Flickr api
+            return tags.Replace(" ", ",");
+        }
+
+        private string cleanSQL(string str)
+        {
+            return str.Replace("'", "''");;
+        }
+
+        protected void Button1_Click(object sender, CommandEventArgs e)
+        {
+            string link = linkHidden.Text;
+            string thumb = thumbHidden.Text;
+            link = cleanSQL(link);
+            thumb = cleanSQL(thumb);
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DB_42245_imagetagConnectionString1"].ConnectionString);
+            SqlCommand nonqueryCommand = conn.CreateCommand();
+
+            try
+            {
+                // Open Connection
+                conn.Open();
+
+                // Create INSERT statement with named parameters
+                nonqueryCommand.CommandText = "INSERT  INTO saves (image, mainimg, tag) VALUES (@image, @link, null)";
+
+                // Add Parameters to Command Parameters collection
+                nonqueryCommand.Parameters.Add("@image", SqlDbType.NVarChar, 100);
+                nonqueryCommand.Parameters.Add("@link", SqlDbType.NVarChar, 100);
+                nonqueryCommand.Parameters["@image"].Value = thumb;
+                nonqueryCommand.Parameters["@link"].Value = link;
+                nonqueryCommand.ExecuteNonQuery();
+
+            }
+
+            catch (SqlException ex)
+            {
+                // Display error
+            }
+            finally
+            {
+                conn.Close();
+            }
+            GridView1.DataBind();
+        }
+
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
